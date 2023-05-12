8d283f345ba3941fd6a8f6cdba5147698c426a40
ktkalpha <ktkalpha@naver.com>
2023-05-12 19:39:04 +0900

    ranking and reset every month

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,29 @@ function App() {
   const [count, setCount] = useState(0);
   const [gc, setGC] = useState("");
   const db = getFirestore(firebase);
+  const [clsc, setClsc] = useState([]);
   async function getClick(db: any, grade: number) {
     const clickCol = collection(db, "click");
     const clickSnap = await getDocs(clickCol);
     const clickData = clickSnap.docs.map((doc) => doc.data());
     return clickData[grade - 1];
   }
+  function resetCheck() {
+    const day = new Date();
+    getClick(db, 4).then((r) => {
+      if (day.getMonth() !== r.month) {
+        getClick(db, 1).then((r) => {
+          let cr = r;
+          for (var i in cr) {
+            cr[i] = 0;
+          }
+          setDoc(doc(db, "click", "grade1"), cr);
+          setDoc(doc(db, "click", "month"), { month: day.getMonth() });
+        });
+      }
+    });
+  }
+  resetCheck();
   function clickHandler() {
     if (gc !== "") {
       setCount(count + 1);
@@ -33,14 +50,15 @@ function App() {
   }
   function changeHandler(e: any) {
     setGC(e.target.value);
-    getClick(db, Number(e.target.value.slice(0, 1))).then((r) =>
-      setCount(r[e.target.value])
-    );
+    getClick(db, Number(e.target.value.slice(0, 1))).then((r) => {
+      setCount(r[e.target.value]);
+      console.log(r);
+    });
   }
   return (
     <div className="flex select-none flex-col">
       <button
-        className="bg-slate-700"
+        className="bg-slate-200 dark:bg-slate-700"
         onClick={() => {
           document.querySelector("#desc")?.classList.toggle("hidden");
         }}
@@ -66,15 +84,46 @@ function App() {
           {count}
         </p>
       </div>
-      <div className="flex mt-32 justify-center items-center">
-        <img onClick={clickHandler} src={logo}></img>
-      </div>
-      <div className="flex space-y-2 mt-20 flex-col justify-center items-center">
+      <div className="flex space-y-2 flex-col justify-center items-center">
         <label>연타 박스(키보드 입력)</label>
         <input
+          className="border-black border-2"
           onKeyDown={clickHandler}
           onChange={(e) => (e.target.value = "")}
         ></input>
+      </div>
+      <div className="flex mt-20 justify-center items-center">
+        <img onClick={clickHandler} src={logo}></img>
+      </div>
+      <div
+        id="modal"
+        className="bg-white text-black shadow-lg rounded-lg duration-200 fixed top-[95vh] h-full w-full"
+      >
+        <p
+          onClick={() => {
+            const modal = document.querySelector("#modal");
+            modal?.classList.toggle("top-[50vh]");
+            modal?.classList.toggle("top-[95vh]");
+            getClick(db, 1).then((r) => {
+              const res = Object.entries(r)
+                .sort(([, a], [, b]) => a - b)
+                .reverse();
+
+              setClsc(res);
+            });
+          }}
+          className="text-lg text-center cursor-pointer border-2 w-full"
+        >
+          △ 순위 ▽
+        </p>
+        <ul className="pl-2 list-none divide-gray-400 divide-y">
+          {clsc.map((v, i) => (
+            <li className="text-lg" key={i}>{`${i + 1}위 ${v[0].slice(
+              0,
+              1
+            )}학년 ${v[0].slice(1)}반 : ${v[1]}`}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
